03b6d8babded1fb500df35a5cf9deb625f4d5b81
Sai Prashanth <65449720+Sai-Prashanth-Banda@users.noreply.github.com>
2021-06-29 12:03:39 +0530

    Add files via upload

---
+++ Frontend/double and triple equals.js
@@ -0,0 +1,4 @@
+var a=parseInt(prompt("enter the value to compare and check u r other value to be given using == and ==="));
+var b=parseInt(prompt("enter the other value to compare and check u r previous value to be given using == and ==="));
+console.log("using == operator the outpur is"+(a==b))
+console.log("using === operator the output is"+(a===b))
