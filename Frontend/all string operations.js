d2b566d18fe09f8026859a42593abe9af45afd3e
Sai Prashanth <65449720+Sai-Prashanth-Banda@users.noreply.github.com>
2021-07-03 14:51:01 +0530

    Add files via upload

---
+++ Frontend/all string operations.js
@@ -0,0 +1,8 @@
+var a=prompt("enter a string and see all operations in console")
+
+console.log("value of the string is",a);
+console.log("3rd position character from your string is",a.charAt(3));
+console.log("code at 2nd index is",a.charCodeAt(2));
+var b="education"
+console.log("concatination of typed string with education",a.concat(b));
+console.log("length of the string is",a.length);
